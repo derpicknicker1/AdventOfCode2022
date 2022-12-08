c52121a76093fba14cc292a8fd6336ca3b68681a
derpicknicker1 <10minutemail@web.de>
2022-12-08 19:00:30 +0100

    Solution Day 8

--- benchmark.bat
+++ benchmark.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Building executeables...
 if not exist "benchmark" mkdir benchmark
-set start=1
+set start=7
 set end=7
 for /l %%x in (%start%, 1, %end%) do (   
 	echo Building Day %%x
@@ -11,9 +11,10 @@ for /l %%x in (%start%, 1, %end%) do (
     echo get%%xa("input/%%x.txt"^);get%%xb("input/%%x.txt"^);return 0;} ) >benchmark/main.c
    gcc -o benchmark/adv%%x benchmark/main.c src/advent%%x.c src/helper.c -I src/
 )
-
+@REM SETLOCAL
+@REM SET PATH=%PATH%;C:\Users\Flow\OneDrive\Dokumente\GitHub\AdventOfCode2022\benchmark
 echo Starting benchmarks...
 .\hyperfine\hyperfine.exe -w 30 --export-markdown benchmark/out.md -P num %start% %end% "benchmark\adv{num}.exe"
-
+@REM ENDLOCAL
 echo &echo,&echo,Results:
 type benchmark\out.md
